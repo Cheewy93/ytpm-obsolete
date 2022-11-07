285e528c136f8d4021cbfd118b0e55ebadaf6b91
Cheewy93 <cheewy93@gmail.com>
2022-11-07 22:20:44 +0100

    Updates

---
+++ precache-manifest.d1acc28b0c9ab8ee1603a8055ea26eec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c918b4201283b474020dfe4d47f9130",
+    "url": "/ytpm-obsolete/index.html"
+  },
+  {
+    "revision": "1c0eba5341c4cae81471",
+    "url": "/ytpm-obsolete/static/css/main.122e4116.chunk.css"
+  },
+  {
+    "revision": "f29d3e4b6b7a403909db",
+    "url": "/ytpm-obsolete/static/js/2.c91a94f2.chunk.js"
+  },
+  {
+    "revision": "71547f7c357d5324e68e6d2a41606daf",
+    "url": "/ytpm-obsolete/static/js/2.c91a94f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c0eba5341c4cae81471",
+    "url": "/ytpm-obsolete/static/js/main.01619571.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/ytpm-obsolete/static/js/main.01619571.chunk.js.LICENSE"
+  },
+  {
+    "revision": "783c3c6ad00c9e72b1a5",
+    "url": "/ytpm-obsolete/static/js/runtime-main.fef52acf.js"
+  },
+  {
+    "revision": "70392d3b719f46cda692930bf3b61a43",
+    "url": "/ytpm-obsolete/static/media/loading.70392d3b.gif"
+  },
+  {
+    "revision": "ba6b12ca852d7c9f0a317e9fefd02a89",
+    "url": "/ytpm-obsolete/static/media/logo.ba6b12ca.png"
+  }
+]);
